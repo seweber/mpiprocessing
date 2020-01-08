8357ba84ed9da6b4e0abf55eac3552277c8ddc7f
seweber <seweber@posteo.de>
2020-01-08 13:52:30 +0100

    Use appveyor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'mpimap',
     packages = find_packages(include=['mpimap']),
-    install_requires = ['mpi4py', 'cloudpickle'],
+    install_requires = ['mpi4py', 'cloudpickle', 'numpy'],
     setup_requires = ['pytest-runner'],
     tests_require = ['pytest'],
 )
