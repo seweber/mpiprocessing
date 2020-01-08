a0ec7a566f2a3218e6495dd1e4f11d559fc2eae1
seweber <seweber@posteo.de>
2020-01-08 11:28:50 +0100

    Use travis

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,6 +5,6 @@ class BasicTest(unittest.TestCase):
     def test_pool(self):
         def fct(idx):
             return idx
-        with Pool() as p:
+        with Pool(2) as p:
             results = p.map(fct, range(1000))
         self.assertEqual(results, list(range(1000)))
